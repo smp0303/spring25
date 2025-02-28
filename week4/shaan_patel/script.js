3cc2b6187801459db1dfa696d8f5af52da40f8e4
smp0303 <shaanpatel@berkeley.edu>
2025-02-27 20:23:32 -0800

    updated cookie clicker

---
+++ week4/shaan_patel/script.js
@@ -0,0 +1,7 @@
+function addOne(){
+    document.getElementById("counter").innerText = parseInt(document.getElementById("counter").innerText) + 1
+
+}
+function reset(){
+    document.getElementById("counter").innerText = 0
+}
