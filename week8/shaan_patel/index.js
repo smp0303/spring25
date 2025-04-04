0eb58d8051d398d6d190fe277bbf83a07e744877
smp0303 <shaanpatel@berkeley.edu>
2025-04-03 20:14:49 -0700

    attempted mongodb and api

--- week8/shaan_patel/index.js
+++ week8/shaan_patel/index.js
@@ -9,6 +9,46 @@ import { availableParallelism } from 'node:os';
 import cluster from 'node:cluster';
 import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  content: { type: String }
+})
+
+const messageModel = mongoose.model("Message", messageSchema)
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    const message = new messageModel();
+    message.content = msg;
+    message.save().then(m => {
+      io.emit('chat message', msg);
+    })
+  });
+});
+
+server.listen(3000, async function(){
+  await mongoose.connect("mongodb+srv://shaanpatel:LDD5EYRbeERVQWfH@cluster0.65nf7gr.mongodb.net/")
+  console.log('listening on *:3000');
+});
+
+
+app.get('/messages', async function(req, res){
+  res.json(await messageModel.find());
+});
+
 if (cluster.isPrimary) {
   const numCPUs = availableParallelism();
   for (let i = 0; i < numCPUs; i++) {
